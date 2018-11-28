145773ab410dd7c76a233d1d6baf494ddcb6e5c1
DESKTOP-TGFQBC8\Atul <atul.mishra@ticketmaster.co.uk>
2018-11-28 22:44:06 +0000

    Initial

---
+++ sample/DDD/AbstractCore.DDD.Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using AbstractCore.DDD.Core;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace AbstractCore.DDD.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddMediatR(typeof(Ping))
+            .BuildServiceProvider();
+
+            var mediator = serviceProvider.GetService<IMediator>();
+            var response = mediator.Send(new Ping()).Result;
+
+            Console.WriteLine(response);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/DDD/AbstractCore.DDD.Core/AggregateRoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace AbstractCore.DDD.Core
+{
+    public class Command : IRequest<CommandResult> { }
+
+    public class DomainEvent : INotification { }
+
+    public class CommandResult
+    {
+
+    }
+
+    public abstract class AggregateRoot<T> : IRequestHandler<Command, CommandResult>
+    {
+        private readonly IMediator _mediator;
+
+        public AggregateRoot(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<CommandResult> Handle(Command request, CancellationToken cancellationToken)
+        {
+            await _mediator.Publish(new DomainEvent());
+            return new CommandResult();
+        }
+    }
+
+    public abstract class DomainEventHandler<T> : INotificationHandler<DomainEvent>
+    {
+        public Task Handle(DomainEvent notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
